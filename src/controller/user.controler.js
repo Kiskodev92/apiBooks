c119b8f424f909abe7095394f7263b6ab27495fb
Kiskotkd <fjgp92@hotmail.com>
2023-10-13 18:13:33 +0200

    avance api

--- src/controller/user.controler.js
+++ src/controller/user.controler.js
@@ -23,4 +23,25 @@ const registerUser = async (req, res) =>
     }
 }
 
-module.exports = {registerUser};
+function login(request, response){
+    const {email, password} = request.body;
+    const sql = `SELECT * FROM user WHERE email =? AND password = ?`;
+    const params = [email, password];
+    
+    connection.query(sql, params, function (err, result) {
+      if (err) {
+        console.log(err);
+        response.status(500).send("Login incorrecto");
+      } else {
+        console.log(result);
+        if (result.length>0) {
+          console.log(result);
+          response.status(200).json(result);
+        } else {
+          response.status(401).send("Login incorrecto");
+        }
+      }
+    });
+}
+
+module.exports = {registerUser, login};
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,6 +3,7 @@ const router = Router();
 const userCtrl = require("../controller/user.controler");
 
 router.post("/postuser", userCtrl.registerUser);
+router.post("/loginuser", userCtrl.login);
 
 
 module.exports = router;
